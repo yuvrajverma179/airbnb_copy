de9359ac0ef0c0b07b4e87866e2a1c30daecc661
yuvrajverma179 <iitbyuvidk179@gmail.com>
2024-05-22 07:39:17 +0530

    day_two of this project

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ main()
 
 //creation of main function
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/wanderlust');
+    await mongoose.connect
+    ('mongodb://localhost:27017/wanderlust');
 }
 
 app.get("/", (req,res) => {
@@ -32,11 +33,31 @@ app.get("/", (req,res) => {
     res.send("");
 });
 
+//Index Route
 app.get("/listings", async (req, res) => {
-    const allistings = await Listing.find({});
-    res.render("./listings/index.ejs", {allistings});
+    const alllistings = await Listing.find({});
+    res.render("./listings/index.ejs", {alllistings});
 })
 
+//New Route
+app.get("/listings/new", (req, res) => {
+    res.render("./listings/new.ejs");
+});
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/show.ejs", {listing});
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+    console.log(req.body);
+    // const newListing = new Listing(req.body);
+    // await newListing.save();
+    // res.redirect("/listings");
+})
 // app.get("/testlisting", async (req, res) => {
 //     let sampleListing = new Listing({
 //         title: "My Villa",
